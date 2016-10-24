dba405c7af2140b9c1176e6e31a825b3cd912940
viviana rishe <vivianacuri@gmail.com>
2016-10-24 11:54:55 -0700

    generates puppies controller and view templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'puppies/index'
+
+  get 'puppies/new'
+
+  get 'puppies/show'
+
+  get 'puppies/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
